0c0810efa4ff265bd763909d95c34579fe686149
Jon Borgonia <jon@gomagames.com>
2015-06-01 12:31:03 -1000

    adds tests for Eukaryota

--- test/kingdoms_test.js
+++ test/kingdoms_test.js
@@ -9,6 +9,7 @@
 import LivingThing from '../lib/LivingThing';
 import Archaea from '../lib/Archaea';
 import Bacteria from '../lib/Bacteria';
+import Eukaryota from '../lib/Eukaryota';
 
 import 'should';
 
@@ -22,7 +23,7 @@ describe('LivingThing', () => {
 
   describe('constructor', () => {
     
-    it('should have constructor arguments (name, uniCellular, trueNucleus, anaerobic, asexual, mobile) that set respective private variables', () => {
+    it('should have arguments (name, uniCellular, trueNucleus, anaerobic, asexual, mobile) that set respective private variables', () => {
       livingThing._name.should.be.equal('alien');
       livingThing._uniCellular.should.be.false;
       livingThing._trueNucleus.should.be.true;
@@ -44,78 +45,78 @@ describe('LivingThing', () => {
     });
 
     it('should have a method named `uniCellular` that returns a boolean value based on the private variable `_uniCellular`', () => {
-      livingThing._uniCellular.should.be.equal(false);
-      livingThing.uniCellular.should.be.equal(false);
+      livingThing._uniCellular.should.be.false;
+      livingThing.uniCellular.should.be.false;
       livingThing.uniCellular = true;
-      livingThing._uniCellular.should.be.equal(true);
-      livingThing.uniCellular.should.be.equal(true);
+      livingThing._uniCellular.should.be.true;
+      livingThing.uniCellular.should.be.true;
     });
     it('should have a method named `multiCellular` that returns a boolean value based on the private variable `_uniCellular`', () => {
-      livingThing._uniCellular.should.be.equal(false);
-      livingThing.multiCellular.should.be.equal(true);
+      livingThing._uniCellular.should.be.false;
+      livingThing.multiCellular.should.be.true;
       livingThing.multiCellular = false;
-      livingThing._uniCellular.should.be.equal(true);
-      livingThing.multiCellular.should.be.equal(false);
+      livingThing._uniCellular.should.be.true;
+      livingThing.multiCellular.should.be.false;
     });
 
     it('should have a method named `eukaryote` that returns a boolean value based on the private variable `_trueNucleus`', () => {
-      livingThing._trueNucleus.should.be.equal(true);
-      livingThing.eukaryote.should.be.equal(true);
+      livingThing._trueNucleus.should.be.true;
+      livingThing.eukaryote.should.be.true;
       livingThing.eukaryote = false;
-      livingThing._trueNucleus.should.be.equal(false);
-      livingThing.eukaryote.should.be.equal(false);
+      livingThing._trueNucleus.should.be.false;
+      livingThing.eukaryote.should.be.false;
     });
     it('should have a method named `prokaryote` that returns a boolean value based on the private variable `_trueNucleus`', () => {
-      livingThing._trueNucleus.should.be.equal(true);
-      livingThing.prokaryote.should.be.equal(false);
+      livingThing._trueNucleus.should.be.true;
+      livingThing.prokaryote.should.be.false;
       livingThing.prokaryote = true;
-      livingThing._trueNucleus.should.be.equal(false);
-      livingThing.prokaryote.should.be.equal(true);
+      livingThing._trueNucleus.should.be.false;
+      livingThing.prokaryote.should.be.true;
     });
 
     it('should have a method named `anaerobic` that returns a boolean value based on the private variable `_anaerobic`', () => {
-      livingThing._anaerobic.should.be.equal(false);
-      livingThing.anaerobic.should.be.equal(false);
+      livingThing._anaerobic.should.be.false;
+      livingThing.anaerobic.should.be.false;
       livingThing.anaerobic = true;
-      livingThing._anaerobic.should.be.equal(true);
-      livingThing.anaerobic.should.be.equal(true);
+      livingThing._anaerobic.should.be.true;
+      livingThing.anaerobic.should.be.true;
     });
     it('should have a method named `aerobic` that returns a boolean value based on the private variable `_anaerobic`', () => {
-      livingThing._anaerobic.should.be.equal(false);
-      livingThing.aerobic.should.be.equal(true);
+      livingThing._anaerobic.should.be.false;
+      livingThing.aerobic.should.be.true;
       livingThing.aerobic = false;
-      livingThing._anaerobic.should.be.equal(true);
-      livingThing.aerobic.should.be.equal(false);
+      livingThing._anaerobic.should.be.true;
+      livingThing.aerobic.should.be.false;
     });
     
     it('should have a method named `asexual` that returns a boolean value based on the private variable `_asexual`', () => {
-      livingThing._asexual.should.be.equal(true);
-      livingThing.asexual.should.be.equal(true);
+      livingThing._asexual.should.be.true;
+      livingThing.asexual.should.be.true;
       livingThing.asexual = false;
-      livingThing._asexual.should.be.equal(false);
-      livingThing.asexual.should.be.equal(false);
+      livingThing._asexual.should.be.false;
+      livingThing.asexual.should.be.false;
     });
     it('should have a method named `sexual` that returns a boolean value based on the private variable `_asexual`', () => {
-      livingThing._asexual.should.be.equal(true);
-      livingThing.sexual.should.be.equal(false);
+      livingThing._asexual.should.be.true;
+      livingThing.sexual.should.be.false;
       livingThing.sexual = true;
-      livingThing._asexual.should.be.equal(false);
-      livingThing.sexual.should.be.equal(true);
+      livingThing._asexual.should.be.false;
+      livingThing.sexual.should.be.true;
     });
 
     it('should have a method named `mobile` that returns a boolean value based on the private `_mobile`', () => {
-      livingThing._mobile.should.be.equal(true);
-      livingThing.mobile.should.be.equal(true);
+      livingThing._mobile.should.be.true;
+      livingThing.mobile.should.be.true;
       livingThing.mobile = false;
-      livingThing._mobile.should.be.equal(false);
-      livingThing.mobile.should.be.equal(false);
+      livingThing._mobile.should.be.false;
+      livingThing.mobile.should.be.false;
     });
     it('should have a method named `immobile` that returns a boolean value based on the private `_mobile`', () => {
-      livingThing._mobile.should.be.equal(true);
-      livingThing.immobile.should.be.equal(false);
+      livingThing._mobile.should.be.true;
+      livingThing.immobile.should.be.false;
       livingThing.immobile = true;
-      livingThing._mobile.should.be.equal(false);
-      livingThing.immobile.should.be.equal(true);
+      livingThing._mobile.should.be.false;
+      livingThing.immobile.should.be.true;
     });
     
   });
@@ -136,7 +137,7 @@ describe('Archaea', () => {
 
   describe('constructor', () => {
     
-    it('should have constructor arguments (name) that sets the private `_name` value', () => {
+    it('should have arguments (name) that sets the private `_name` value', () => {
       archaea._name.should.be.equal('Acidilobus saccharovorans');
     });
 
@@ -166,7 +167,7 @@ describe('Bacteria', () => {
 
   describe('constructor', () => {
     
-    it('should have constructor arguments (name) that sets the private `_name` value', () => {
+    it('should have arguments (name) that sets the private `_name` value', () => {
       bacteria._name.should.be.equal('Clostridium');
     });
 
@@ -178,6 +179,57 @@ describe('Bacteria', () => {
       bacteria._mobile.should.be.false;
     });
     
+  });
+
+});
+
+describe('Eukaryota', () => {
+  
+  let eukaryota;
+
+  beforeEach(() => {
+    eukaryota = new Eukaryota('Yeast', false, true, false, true);
+  });
+
+  it('should extend LivingThing', () => {
+    (eukaryota instanceof LivingThing).should.be.true;
+  });
+
+  describe('constructor', () => {
+    
+    it('should have arguments (name, uniCellular, asexual, mobile, heterotroph) that set respective private variables', () => {
+      eukaryota._name.should.be.equal('Yeast');
+      eukaryota._uniCellular.should.be.false;
+      eukaryota._asexual.should.be.true;
+      eukaryota._mobile.should.be.false;
+      eukaryota._heterotroph.should.be.true;
+    });
+
+    it('should invoke super constructor with correct hardcoded arguments', () => {
+      eukaryota._uniCellular.should.be.false;
+      eukaryota._trueNucleus.should.be.true;
+      eukaryota._anaerobic.should.be.false;
+      eukaryota._asexual.should.be.true;
+      eukaryota._mobile.should.be.false;
+    });
+    
+  });
+
+  describe('getters and setters', () => {
+    it('should have a method named `heterotroph` that returns a boolean value based on the private variable `_heterotroph`', () => {
+      eukaryota._heterotroph.should.be.true;
+      eukaryota.heterotroph.should.be.true;
+      eukaryota.heterotroph = false;
+      eukaryota._heterotroph.should.be.false;
+      eukaryota.heterotroph.should.be.false;
+    });
+    it('should have a method named `autotroph` that returns a boolean value based on the private variable `_heterotroph`', () => {
+      eukaryota._heterotroph.should.be.true;
+      eukaryota.autotroph.should.be.false;
+      eukaryota.autotroph = true;
+      eukaryota._heterotroph.should.be.false;
+      eukaryota.autotroph.should.be.true;
+    });
   });
 
 });
